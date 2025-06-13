9e99f9f0330f4157e663e89ce5117d170dbc7a66
Darshan Raju <d.raju@tudelft.nl>
2025-06-13 13:14:28 +0200

    Delete GIT.sh

--- GIT.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git init
-git add *
-git commit -m "PPT Added"
-# git branch -M main
-# git remote add origin https://github.com/Darz2/Summer_School_polimi.git
-git push -u origin main
