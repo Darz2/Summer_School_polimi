831d5ed87a92858a4e3f899d6d46709857280473
Darz2 <d.raju@tudelft.nl>
2025-06-13 13:09:02 +0200

    PPT Added

--- GIT.sh
+++ GIT.sh
@@ -2,7 +2,7 @@
 
 git init
 git add *
-git commit -m "Exercise files-creation"
+git commit -m "PPT Added"
 # git branch -M main
 # git remote add origin https://github.com/Darz2/Summer_School_polimi.git
 git push -u origin main
